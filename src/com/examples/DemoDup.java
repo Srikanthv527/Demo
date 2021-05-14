a8ca6e75cded2105321ba3930e5b32843cfe48bf
Srikanth <Srikanthv527@gmail.com>
2021-05-14 02:41:37 -0500

    C1

--- src/com/examples/DemoDup.java
+++ src/com/examples/DemoDup.java
@@ -3,7 +3,7 @@ package com.examples;
 public class DemoDup extends Demo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Demo DUP");
 
 	}
 
